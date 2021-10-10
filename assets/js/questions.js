0b0401be2cd6b2a47db5e9b069876848acaae455
Sam Davenport <samueldavenport@att.net>
2021-10-10 08:37:43 -0500

    Added questions.js and put an array of 10 questions in it, linked it to index.html

---
+++ assets/js/questions.js
@@ -0,0 +1,64 @@
+var questionsArr = [{
+    content: "An object is an unordered list of________.",
+    answer1: "Values",
+    answer2: "Names",
+    answer3: "Properties",
+    answer4: "All of the Above"
+}, {
+    content: "What stores data value and can be changed later on?",
+    answer1: 'node',
+    answer2: 'console.log',
+    answer3: 'variable',
+    answer4: 'class name'
+}, {
+    content:'How many ways are there with which to declare a variable in javaScript?' ,
+    answer1: 'Only One',
+    answer2: 'Three',
+    answer3: 'Infinitely Many',
+    answer4: 'None of the Above'
+}, {
+    content: 'A block of code designed to perform a particular task.',
+    answer1: 'scope',
+    answer2: 'function',
+    answer3: 'iterator',
+    answer4: 'string'
+}, {
+    content: 'Inside which html element do we put the javaScript?',
+    answer1: '<javacript>',
+    answer2: '<js>',
+    answer3: '<src>',
+    answer4: '<script>'
+}, {
+    content: 'Allows javaScript sites and apps to save key-value pairs in a web browser.',
+    answer1: 'localStorage',
+    answer2: 'constant',
+    answer3: 'repository',
+    answer4: 'github' 
+}, {
+    content: 'What is the first step in the process of saving work to a remote github repository?',
+    answer1: 'git push',
+    answer2: 'git pull',
+    answer3: 'git checkout',
+    answer4: 'git add -A'
+}, {
+    content: 'Which of the following represents loops in javaScript?',
+    answer1: 'for',
+    answer2: 'while',
+    answer3: 'forEach',
+    answer4: 'All of the Above'
+}, {
+    content: 'Which method is used to insert a new element at the end of an array?',
+    answer1: 'unshift()',
+    answer2: 'push()',
+    answer3: 'pop()',
+    answer4: 'None of the Above'
+}, {
+    content: 'Arrays in javaScript are declared using ______.',
+    answer1: 'curly brackets {}',
+    answer2: 'square brackets []',
+    answer3: 'parenthesis ()',
+    answer4: 'None of the Above'
+}
+    
+
+];
